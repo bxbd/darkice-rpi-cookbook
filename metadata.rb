308bd0731d6cf10176dcc27100c7cc053ee761bd
Tim Petter <tim@timpetter.de>
2015-06-05 21:20:14 +0200

    Add recipes to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures darkice on a raspberry pi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 
+recipe 'darkice-rpi::default', 'Install precompiled package'
+recipe 'darkice-rpi::source', 'Install from source'
+
 depends 'apt'
 
 supports 'debian'
